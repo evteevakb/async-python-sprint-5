454476c7b03cd71260a2df4525212a4bec7151fe
Ksenia Evteeva <k.evteeva@skoltech.ru>
2023-11-15 23:38:14 +0300

    Add cascade delete to the database

--- src/models/models.py
+++ src/models/models.py
@@ -1,5 +1,6 @@
 """Database models"""
 from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.orm import backref, relationship
 
 from core.config import app_settings
 from models.base import Base
@@ -10,20 +11,26 @@ class Users(Base):
     __tablename__ = 'users'
     username = Column(String(app_settings.max_username_length), primary_key=True)
     password = Column(String(app_settings.max_password_length), nullable=False)
+    tokens = relationship('Tokens', back_populates='token_relationship',
+                          cascade='all, delete', passive_deletes=True)
+    files = relationship('Files', back_populates='files_relationship',
+                         cascade='all, delete', passive_deletes=True)
 
 
 class Tokens(Base):
     """Model of the 'tokens' table"""
     __tablename__ = 'tokens'
-    username = Column(String(app_settings.max_username_length), ForeignKey('users.username'),
-                      primary_key=True)
+    username = Column(String(app_settings.max_username_length),
+                      ForeignKey('users.username', ondelete='CASCADE'), primary_key=True)
     token = Column(String(app_settings.max_token_length), nullable=False, unique=True)
+    token_relationship = relationship('Users', back_populates='tokens')
 
 
 class Files(Base):
     """Model of the 'files' table"""
     __tablename__ = 'files'
     id = Column(Integer, primary_key=True)
-    username = Column(String(app_settings.max_username_length), ForeignKey('users.username'),
-                      nullable=False)
+    username = Column(String(app_settings.max_username_length),
+                      ForeignKey('users.username', ondelete='CASCADE'), nullable=False)
     filepath = Column(String(app_settings.max_filepath_length), nullable=False, unique=True)
+    files_relationship = relationship('Users', back_populates='files')
